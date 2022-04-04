946ff16632cfa417749fb11937c9d948ae141c8b
HydraSpex <info@hydraspex.com>
2022-04-04 22:27:41 +0200

    Update MCP4151_1.py

--- MCP4151_1.py
+++ MCP4151_1.py
@@ -9,7 +9,7 @@ class MCP4151_0(object):
         self.spi.open(0, 0)
         self.spi.max_speed_hz = 976000                          #Set to preferred Speed
 
-    def write_pot(self, input):
+    def write_pot(self, input):                                 #Input can be 0-255
         msb = input >> 8
         lsb = input & 0xFF
         self.spi.xfer([msb, lsb])
