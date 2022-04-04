74269c7cb395dab35a4282a307fb2c78520e1dac
HydraSpex <info@hydraspex.com>
2022-04-04 23:28:39 +0200

    Update and rename Test_0.py to Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from MCP4151_lib import MCP4151_0                   #Import MCP4151_0 for connection on CE0 and MCP4151_1 for connection on CE1
-Poti = MCP4151_0()
-Poti.write_pot(0)                                 #Input can be 0-255
+Poti = MCP4151_0()                                  #Use MCP4151_0 for connection on CE0 and MCP4151_1 for connection on CE1
+Poti.write_pot(0)                                   #Input can be 0-255
 
 while True:
     i = 0
