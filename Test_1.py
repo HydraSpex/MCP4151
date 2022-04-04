d972aa564ec5f0cef6e6f3b1fdb115c73ea736b7
HydraSpex <info@hydraspex.com>
2022-04-04 22:31:23 +0200

    Update Test_1.py

--- Test_1.py
+++ Test_1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from MCP4151_1 import MCP4151_1
+from MCP4151_1 import MCP4151_1                   #Import MCP4151_0 for connection on CE0 and MCP4151_1 for connection on CE1
 Poti = MCP4151_1()
-Poti.write_pot(0)
+Poti.write_pot(0)                                 #Input can be 0-255
 
 while True:
     i = 0
